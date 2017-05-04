69314102b4286b122948625ada005fb72e05b396
San Pwint Thu <bplusmyanmar9@hotmail.com>
2017-05-04 17:03:26 +0630

    update

--- Modules/Theme/Resources/views/bptheme1/layouts/footer.blade.php
+++ Modules/Theme/Resources/views/bptheme1/layouts/footer.blade.php
@@ -1,5 +1,4 @@
 <div class="copyright col-md-12">
-    <div>
       <div class="col-md-1"> </div>
       <div class="col-md-5 copyright1">
         <h1> Connect with us </h1>
@@ -9,37 +8,32 @@
              <li><a href="#"><img src="{{ asset("/public/assets/bptheme1/img/twitter1.png") }}"></a></li>
              <li><a href="#"><img src="{{ asset("/public/assets/bptheme1/img/youtube.png") }}"></a></li>
          </ul>
-        </div>
-       <div class="col-md-2 copyright2">
       </div>
-        <div class="col-md-4 copyright3">
+      <div class="col-md-2 copyright2">
+      </div>
+      <div class="col-md-4 copyright3">
           <h1> Contact Us </h1>
         78, 6(B) Theingyi Street, Kyeemyindine, Yangon<br />
         bplusmyanmar@hotmail.com<br />
         09 3133 0633, 09 2522 32135
       </div>
+</div>
+<div class="col-md-12">
+  <div class="col-md-1"> </div>
+  <div class="col-md-4 ">
 
-      </div>
-
-      <div>
-      <div class="col-md-1"> </div>
-      <div class="col-md-4 ">
-
-          <p style="clear:both;">Copyright © Beyond Plus 2016 - {{ date('Y') }}, all rights reserved</p>
-      </div>
-       <div class="col-md-3 nr_left">
-          <ul>
-                 <li><a href="#"> Home</a></li>
-                 <li><a href="#"> About</a></li>
-                 <li><a href="#"> Activity </a></li>
-                 <li><a href="#"> Contact</a></li>
-                 <li><a href="#"> Guide</a></li>
-          </ul>
-      </div>
-        <div class="col-md-4 ">
-            </div>
-
-      </div>
+      <p >Copyright © Beyond Plus 2016 - {{ date('Y') }}, all rights reserved</p>
+  </div>
+  <div class="col-md-3 nr_left">
+      <ul>
+             <li><a href="#"> Home</a></li>
+             <li><a href="#"> About</a></li>
+             <li><a href="#"> Activity </a></li>
+             <li><a href="#"> Contact</a></li>
+             <li><a href="#"> Guide</a></li>
+      </ul>
+  </div>
+  <div class="col-md-4 "></div>
 </div>
 
 
